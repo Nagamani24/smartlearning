1d9c6bcc7c2491699e27c958f427a7d07d27b167
Nagamani24 <b.n.mani2427@gmail.com>
2023-09-16 14:37:30 +0530

    smart learning

---
+++ smart-learning/pages/index.vue
@@ -0,0 +1,11 @@
+
+<template>
+<div class="bg-fixed bg-cover bg-center bg-blend-saturation" style="background-image: url('https://media.istockphoto.com/id/948623158/vector/seamless-innovation-and-scientific-data-background.jpg?s=612x612&w=0&k=20&c=IHOut6hIYjxiY5MZlu1gDd4_K886MhfVpfUQxYLr16E=');">
+  <div class="flex flex-col justify-center items-center text-center h-screen w-full">
+        <h1 class="font-semibold text-4xl font-serif text-white"> Welcome to <br /><span class="italic text-amber-500">SMART LEARNING</span></h1>
+        <p class="text-2xl mx-5 text-white">Start your learning with less time and understand the concepts easily.</p>
+        <NuxtLink to="/subjects"><button class="text-xl font-semibold bg-transparent border border-white rounded-lg px-4 py-2 mt-5 text-white font-sans">Let's Start
+        </button></NuxtLink>
+    </div>
+    </div>
+</template>
---
+++ smart-learning/pages/subjects.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="h-screen w-full p-8 bg-slate-100">
+<div class="flex flex-col justify-start">
+    <div class="font-roboto">
+        <h1 class="text-lg text-slate-800">See the list of subjects available</h1>
+        <p class="text-sm text-slate-400">Click on the subject you want to learn</p>
+    </div>
+    <div>
+        <NuxtLink>
+            <button class="bg-blue-400 w-full py-2 m-3 ml-0 rounded-xl text-white text-md ">
+                Engineering Physics
+            </button>
+        </NuxtLink>
+    </div>
+    </div>
+</div>
+    
+</template>
